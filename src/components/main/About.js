e3de6345ac54ff7f7f64ca3d11a1c64177465926
Takako Onodera <takako.onodera@gmail.com>
2025-03-30 16:07:11 +0200

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	new file:   public/sitemap.xml 	modified:   src/components/main/About.js 	modified:   src/i18n.js 	modified:   src/index.js

--- src/components/main/About.js
+++ src/components/main/About.js
@@ -2,33 +2,53 @@ import React from 'react'
 import { Container, Row, Col } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import Figure from 'react-bootstrap/Figure';
+import { Helmet } from 'react-helmet-async';
+
+
 
 const About = () => {
   const { t } = useTranslation();  
+
   return (
-    <div className="about_me-section mt-2" id="about">
-        <h1 className="text-secondary text-center">{t("about_me")}</h1>
-   
-        <Container className="d-flex justify-content-center my-5 aboutMe">
-            <Row className="d-flex">
-                <Col className="mx-auto" lg="2">
-                    <Figure>
-                        <Figure.Image
-                            width={171}
-                            height={171}
-                            alt="My Avatar"
-                            src="images/myavatar.png"
-                            className="rounded-circle border border-white"
-                        />
-                    </Figure>    
-                </Col>
-                <Col className="" lg="6">
-                    <p className="fs-4">{t("about_me")}:<br></br> <span className="fs-6">{t("about_me_description")}</span></p>
-                    <p className="fs-4">{t("about_portofolio")}: <br></br><span className="fs-6">{t("portofolio_description")}</span></p>
-                </Col>
-            </Row>
-        </Container>
-    </div>
+    <>
+      {/* ✅ ここに Helmet を追加 */}
+      <Helmet>
+        <title>About | My Portfolio</title>
+        <meta
+            name="description"
+            content="This is a personal portfolio website built with React. It showcases my web development skills and project experience. Recommended for those looking for examples of React-based portfolios."
+        />
+      </Helmet>
+
+      <div className="about_me-section mt-2" id="about">
+          <h1 className="text-secondary text-center">{t("about_me")}</h1>
+    
+          <Container className="d-flex justify-content-center my-5 aboutMe">
+              <Row className="d-flex">
+                  <Col className="mx-auto" lg="2">
+                      <Figure>
+                          <Figure.Image
+                              width={171}
+                              height={171}
+                              alt="My Avatar"
+                              src="images/myavatar.png"
+                              className="rounded-circle border border-white"
+                          />
+                      </Figure>    
+                  </Col>
+                  <Col lg="6">
+                      <p className="fs-4">{t("about_me")}:</p>
+                      {t("about_me_description").split('\n').map((line, index) => (
+                          <p className="fs-6 mb-2" key={index}>{line}</p>
+                      ))}
+
+                      <p className="fs-4 mt-4">{t("about_portofolio")}:</p>
+                      <p className="fs-6">{t("portofolio_description")}</p>
+                  </Col>
+              </Row>
+          </Container>
+      </div>
+    </>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import "./App.css";
-
+import { HelmetProvider } from 'react-helmet-async';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <HelmetProvider>
     <App />
-  </React.StrictMode>
+  </HelmetProvider>
 );
 
 
